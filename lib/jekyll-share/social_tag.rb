db1e8e89b1dc9484ba5f738f858a32020c88116a
renshuki <renshuki@mail.com>
2017-08-31 00:26:28 +0900

    Facebook share link improvement

--- lib/jekyll-share/social_tag.rb
+++ lib/jekyll-share/social_tag.rb
@@ -5,6 +5,7 @@ module Jekyll
       def render(context)
         site_url = context.registers[:site].config['url']
         site_twitter_username = context.registers[:site].config['twitter_username']
+        site_facebook_app_id = context.registers[:site].config['facebook']['app_id']
         page_url = context.registers[:page]['url']
         page_title = context.registers[:page]['title']
         full_url = site_url + page_url
@@ -12,7 +13,7 @@ module Jekyll
         output = ""
         output << "<ul>"
         output << "<li><a href='https://twitter.com/intent/tweet?text=#{page_title}&url=#{full_url}&via=#{site_twitter_username}&related=#{site_twitter_username}' rel='nofollow' target='_blank' title='Share on Twitter'>Twitter</a></li>"
-        output << "<li><a href='https://facebook.com/sharer.php?u=#{full_url}&title=#{page_title}' rel='nofollow' target='_blank' title='Share on Facebook'>Facebook</a></li>"
+        output << "<li><a href='https://www.facebook.com/dialog/feed?app_id=#{site_facebook_app_id}&link=#{full_url}' rel='nofollow' target='_blank' title='Share on Facebook'>Facebook</a></li>"
         output << "<li><a href='https://plus.google.com/share?url=#{full_url}' rel='nofollow' target='_blank' title='Share on Google+'>Google+</a></li>"
         output << "</ul>"
         output
